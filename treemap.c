c6feae35618403f8121356553ca2f2f9697b9dd2
Your Name <you@example.com>
2024-05-11 15:39:35 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -84,7 +84,7 @@ Pair * searchTreeMap(TreeMap * tree, void* key)
                 tree->current = aux;
                 return aux->pair;
             }
-            if (tree->lower_than(aux->pair->key,key) == 1)
+            if (tree->lower_than(aux->pair->key,key))
             {
                 tree->current = aux;
                 aux = aux->right;
